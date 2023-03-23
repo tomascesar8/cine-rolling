1073a94a3c7b61c8c2d88b68a015137fbb9f90b3
Tomás César <tomascesar1993@gmail.com>
2023-03-23 19:24:37 -0300

    email recuperar contraseña

--- js/email.js
+++ js/email.js
@@ -10,4 +10,116 @@
 //    }).then(message => alert(message)
 //    );
 // }
+function recoveryPass() {
+    let emailUserRecovery = document.querySelector('#email-recovery').value;
+    let users = JSON.parse(localStorage.getItem('users'))
+    let userRecovery = users.find(user=>user.email = emailUserRecovery)
+    console.log();
+}
 
+
+  let suscribe = `
+  <html>
+  <style>
+    body: Verdana, sans-serif;}
+  </style>
+</head>
+<body style="margin:0;padding: 0;">
+  <table class="container-table" role="presentation" style="width: 100%;border-collapse:collapse;border:0;border-spacing:0;background:#ffffff" >
+    <tr>
+      <td align="center" style="padding:0">
+        <table role="presentation" style="width:602px;border-collapse:collapse;border:1px solid #ffffff;border-spacing:0;text-align:left;">
+          <tr>
+            <td align="center" style="padding:40px 35px;">
+              <table role="presentation" style="width:100%;border-collapse:collapse;border:0;border-spacing:0;">
+                <tr>
+                  <td align="center" style="padding:30px 0px;">
+                    <img width="310px" height="auto"src="https://i.ibb.co/whQXZxY/logo-cinerolling.jpg" alt="logo-cinerolling">
+                  </td>
+                </tr>
+                <tr>
+                  <td align="center" style="padding: 0 0 50px 0;">
+                    <h1 style="color:"gainsboro";font-size: large; font-weight:600; margin: 20px 0;">
+                      Bienvenid${userRecovery.gender == "Masculino"? 'o' : userRecovery.gender == "Femenino"? 'a' : 'x'} a <span style="font-weight:700; color:rgb(15, 59, 95)">CineRolling+</span> ! 
+                    </h1>
+                  </td>
+                </tr>
+                <tr>
+
+                Hola ${userRecovery.name}, 
+                recibimos una solicitud para recuperar tu contraseña; si no lo hiciste desestima este e-mail.
+                <br/> <br>
+                Para proceder con la recuperacion presiona el boton que esta debajo, y sigue las instrucciones.  
+                 <br /> <br> Recorda ModoGamer nunca va a solicitarte esta u otra informacion sobre tu cuenta.<br />
+                
+                  <td align="center" style="padding:0;">
+                    <p style="margin: 0 0 20px 0; line-height: 1.2em;">
+                      Hola <span style="font-weight:700">${capitalizeWords(newUser.name)}</span>, recibimos una solicitud para recuperar tu contraseña; si no lo hiciste desestima este e-mail.
+                      <br>
+                      Tu clave de usuario es la siguiente:
+                    </p>
+                  </td>
+                    <tr><td style="background-color:#364852; padding: 1px; "></td></tr>
+                </tr>
+                <tr>
+                  <td align="center" style="padding:40px 0 0 0;">
+                    <a href="#" type="button" style="background-color:#96cae7;text-decoration:none; color:#ffffff; padding: 10px; border-radius:20px">
+                      <button type="button" style="background-color:#96cae7;text-decoration:none; border: none; color:#ffffff; padding: 10px; border-radius:20px">
+                        ${userRecovery.pass}
+                      </button>
+                    </a>
+                  </td>
+                </tr>
+                <tr>
+                  <td align="center" style="padding: 40px 0 0 0;">
+                    <p style="font-size: small; text-align:center;margin: 0 0 20px 0; "> 
+                      Si el botón no funciona, hace click en el siguiente enlace: <a href="#">Reintentar validación</a> 
+                    </p>
+                  </td>
+                </tr>
+              </table>
+            </td>
+          </tr>
+          <tr>
+            <td style="background:#537aa3;padding:15px 30px; border-end-start-radius:15px; border-end-end-radius:15px">
+            <table role="presentation" style="width:100%;border-collapse:collapse;border: 0;border-spacing:0;">
+            <tr>
+              <td style="padding:0;width:42%;" align="left">
+                <h4 style="color:#96cae7">CineRolling+</h4>
+              </td>
+              <td style="padding:0;width:58%;" align="left">
+                <table role="presentation" style="border-collapse:collapse;border:0;border-spacing:0;">
+                  <tr>
+                    <td align="center"style="padding:0 0 0 16px;width:36px;">
+                      <a href="http://www.twitter.com/">
+                        <img src="https://i.ibb.co/2MG0xbd/gorjeo.png" alt="logo-twitter" width="38" style="height:auto;display:block;border:0;" /> 
+                      </a>
+                    </td>
+                    <td align="center"style="padding:0 0 0 16px;width:36px;">
+                      <a href="https://github.com/tomascesar8/cine-rolling">
+                        <img src="https://i.ibb.co/48WGS5q/github-7.png" alt="logo-github" width="36" style="height:auto;display:block;border:0;" /> 
+                      </a>
+                    </td>
+                    <td align="center" style="padding:0 0 0 10px;width:38px;">
+                      <a href="http://www.facebook.com/">
+                        <img src="https://i.ibb.co/7zMxkKG/logo-de-facebook.png" alt="logo-Facebook" width="38" style="height:auto;display:block;border:0;" />
+                      </a>
+                    </td>
+                  </tr>
+                </table>
+              <td style="padding-right:25px; align:right">
+                <a href="#" style="text-decoration:none; color:#96cae7;"><h4>Ayuda</h4></a>
+              </td>
+              </td>
+            </tr>
+          </table>
+            </td>
+          </tr>
+        </table>
+      </td>
+    </tr>
+  </table>
+</body>
+</html>
+`
+{/* <a href="https://imgbb.com/"><img src="https://i.ibb.co/whQXZxY/logo-cinerolling.jpg" alt="logo-cinerolling" border="0"></a> */}
--- js/landing-page.js
+++ js/landing-page.js
@@ -95,31 +95,6 @@ function capitalizeWords(string) {
   return words.join(' ');
 }
 
-function prueba() {
-  const Toast = Swal.mixin({
-    toast: true,
-    position: 'center',
-    showConfirmButton: false,
-    timer: 10000,
-    timerProgressBar: true,
-    didOpen: (toast) => {
-      toast.addEventListener('mouseenter', Swal.stopTimer)
-      toast.addEventListener('mouseleave', Swal.resumeTimer)
-    }
-  })
-  
-  Toast.fire({
-    showCloseButton: true,
-    background: '#141d32',
-    color: 'white',
-    width: '600px',
-    heightAuto: false,
-    icon: 'success',
-    title: `<h5 class="text-warning">Tus datos se cargaron correctamente!</h5> <p>Te enviamos un email para finalizar tu registro.<br> No te olvides revisar el Spam o correo no deseado.</h4>`
-  })
-}
-
-
 function register(event) {
   event.preventDefault()
   let name = document.getElementById('register-name').value;
@@ -167,17 +142,22 @@ function register(event) {
           <td align="center" style="padding:0">
             <table role="presentation" style="width:602px;border-collapse:collapse;border:1px solid #ffffff;border-spacing:0;text-align:left;">
               <tr>
-                <td style="padding:40px 35px;">
+                <td align="center" style="padding:40px 35px;">
                   <table role="presentation" style="width:100%;border-collapse:collapse;border:0;border-spacing:0;">
                     <tr>
-                      <td style="padding: 0 0 50px 0;">
+                      <td align="center" style="padding:30px 0px;">
+                        <img style="border-radius: 2rem" width="310px" height="auto"src="https://i.ibb.co/whQXZxY/logo-cinerolling.jpg" alt="logo-cinerolling">
+                      </td>
+                    </tr>
+                    <tr>
+                      <td align="center" style="padding: 0 0 50px 0;">
                         <h1 style="color:"gainsboro";font-size: large; font-weight:600; margin: 20px 0;">
                           Bienvenid${newUser.gender == "Masculino"? 'o' : newUser.gender == "Femenino"? 'a' : 'x'} a <span style="font-weight:700; color:rgb(15, 59, 95)">CineRolling+</span> ! 
                         </h1>
                       </td>
                     </tr>
                     <tr>
-                      <td style="padding:0;">
+                      <td align="center" style="padding:0;">
                         <p style="margin: 0 0 20px 0; line-height: 1.2em;">
                           Hola <span style="font-weight:700">${capitalizeWords(newUser.name)}</span>, gracias por unirte a nuestra plataforma de video!
                           <br>
@@ -189,8 +169,8 @@ function register(event) {
                     <tr>
                       <td align="center" style="padding:60px 0 0 0;">
                         <a href="#" type="button" style="background-color:#96cae7;text-decoration:none; color:#ffffff; padding: 10px; border-radius:20px">
-                          <button type="button" style="background-color:#96cae7;text-decoration:none; color:#ffffff; padding: 10px; border-radius:20px">
-                            Verifica tu cuenta
+                          <button type="button" style="background-color:#96cae7;text-decoration:none; border: none; color:#ffffff; padding: 10px; border-radius:20px">
+                            Verificar cuenta
                           </button>
                         </a>
                       </td>
@@ -254,7 +234,7 @@ function register(event) {
       Password : "D19478D06661B455EB35D0C3E6D2BCEC7099",
       To : 'tomcesar@hotmail.com',
       From : "tomascesar1993@gmail.com",
-      Subject : `Último paso para confirmar tu nuevo usuario en CineRolling+`,
+      Subject : `Email de verificación de cuenta CineRolling+`,
       Body : suscribe,
     }).then(message => confirm(message));
     
@@ -264,31 +244,33 @@ function register(event) {
       if(response === "OK"){ 
         console.log('Llego a if response async')
           // alert('Llego a if response async')
-          const Toast = Swal.mixin({
-            toast: true,
-            position: 'bottom',
-            showConfirmButton: false,
-            timer: 10000,
-            timerProgressBar: true,
-            didOpen: (toast) => {
-              toast.addEventListener('mouseenter', Swal.stopTimer)
-              toast.addEventListener('mouseleave', Swal.resumeTimer)
-            }
-          })
-          
-          Toast.fire({
-            background: '#6b986b',
-            // color: ,
-            width: '700px',
-            heightAuto: false,
-            icon: 'success',
-            title: `Registro exitoso! Revisá tu casilla de mensajes para finalizar la confirmación.`
-          })
+        const Toast = Swal.mixin({
+          toast: true,
+          position: 'center',
+          showConfirmButton: false,
+          timer: 10000,
+          timerProgressBar: true,
+          didOpen: (toast) => {
+            toast.addEventListener('mouseenter', Swal.stopTimer)
+            toast.addEventListener('mouseleave', Swal.resumeTimer)
+          }
+        })
+        
+        Toast.fire({
+          showCloseButton: true,
+          background: '#0A1A2A',
+          color: 'white',
+          width: '600px',
+          heightAuto: false,
+          icon: 'success',
+          title: `<h5 class="text-warning">Tus datos se cargaron correctamente!</h5> <p>Te enviamos un email para finalizar tu registro.<br> No te olvides revisar el Spam o correo no deseado.</h4>`
+        })
       }else{
           let error = document.createElement('h5');
           error.innerText = 'Ha ocurrido un problema, vuelve a intentarlo.' ;
           console.log('Error en else async');
           alert('Error en else async')
+          console.log(error);
           alert(error)
           // let father = document.querySelector('.register_form');
           // father.appendChild(error);
@@ -406,3 +388,169 @@ const rubberBandBtnSus =()=>{
   }, 6000);
 }
 rubberBandBtnSus()
+
+
+function recoveryPass(event) {
+  event.preventDefault()
+  let emailUserRecovery = document.querySelector('#email-recovery');
+  let users = JSON.parse(localStorage.getItem('users'))
+  let userRecovery = users.find(user=>user.email == emailUserRecovery.value)
+  console.log(userRecovery);
+  if(userRecovery===undefined){
+    Swal.fire({
+      position: 'center',
+      icon: 'error',
+      title: `<h3 style="margin:0" class="text-warning">Dirección de email no registrada</h3>
+              <p style="font-size:18px">El email ingresado no corresponde con ningún usuario. Revisá haberlo escrito correctamente.</p>`,
+      showConfirmButton: false,
+      timer: 6000,
+      showCloseButton: true,
+      background: '#0A1A2A',
+      color: 'white',
+      timerProgressBar: true,
+      width: '600px',
+      didOpen: (toast) => {
+            toast.addEventListener('mouseenter', Swal.stopTimer)
+            toast.addEventListener('mouseleave', Swal.resumeTimer)
+          }
+    })
+  }else{
+    let emailRecovery = `
+      <html>
+      <style>
+        body: Verdana, sans-serif;}
+      </style>
+      </head>
+      <body style="margin:0;padding: 0;">
+        <table class="container-table" role="presentation" style="width: 100%;border-collapse:collapse;border:0;border-spacing:0;background:#ffffff" >
+          <tr>
+            <td align="center" style="padding:0">
+              <table role="presentation" style="width:602px;border-collapse:collapse;border:1px solid #ffffff;border-spacing:0;text-align:left;">
+                <tr>
+                  <td align="center" style="padding:40px 35px;">
+                    <table role="presentation" style="width:100%;border-collapse:collapse;border:0;border-spacing:0;">
+                      <tr>
+                        <td align="center" style="padding:30px 0px;">
+                          <img width="310px" height="auto"src="https://i.ibb.co/whQXZxY/logo-cinerolling.jpg" alt="logo-cinerolling">
+                        </td>
+                      </tr>
+                      <tr>
+                        <td align="center" style="padding: 0 0 20px 0;">
+                          <h1 style="color:"gainsboro";font-size: large; font-weight:600; margin: 20px 0;">
+                            Recuperá tu clave <span style="font-weight:700; color:rgb(15, 59, 95)">CineRolling+</span> ! 
+                          </h1>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td align="center" style="padding:0;">
+                          <p style="margin: 0 0 20px 0; line-height: 1.2em;">
+                            Hola <span style="font-weight:700">${capitalizeWords(userRecovery.name)}</span>, 
+                            recibimos una solicitud para recuperar tu contraseña; si no lo hiciste desestima este e-mail.
+                          </p>
+                        </td>
+                          <tr><td style="background-color:#364852; padding: 1px; "></td></tr>
+                      </tr>
+                      <tr>
+                        <td align="center" style="padding:40px 0 0 0;">                  
+                          <p>Tu clave registrada es: <span style="background-color:#96cae7; color:#ffffff; padding: 10px; border-radius:20px">${userRecovery.pass}</span></p>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td align="center" style="padding: 40px 0 0 0;">
+                          <p>Recorda que CineRolling+ nunca va a solicitarte esta u otra informacion sobre tu cuenta.</p>
+                          <br>
+                          <p style="font-size: small; text-align:center;margin: 0 0 20px 0;"> 
+                            Si deseas modificar tu clave, hace click en el siguiente enlace: <a href="#">Cambiar contraseña</a> 
+                          </p>
+                        </td>
+                      </tr>
+                    </table>
+                  </td>
+                </tr>
+                <tr>
+                  <td style="background:#537aa3;padding:15px 30px; border-end-start-radius:15px; border-end-end-radius:15px">
+                  <table role="presentation" style="width:100%;border-collapse:collapse;border: 0;border-spacing:0;">
+                  <tr>
+                    <td style="padding:0;width:42%;" align="left">
+                      <h4 style="color:#96cae7">CineRolling+</h4>
+                    </td>
+                    <td style="padding:0;width:58%;" align="left">
+                      <table role="presentation" style="border-collapse:collapse;border:0;border-spacing:0;">
+                        <tr>
+                          <td align="center"style="padding:0 0 0 16px;width:36px;">
+                            <a href="http://www.twitter.com/">
+                              <img src="https://i.ibb.co/2MG0xbd/gorjeo.png" alt="logo-twitter" width="38" style="height:auto;display:block;border:0;" /> 
+                            </a>
+                          </td>
+                          <td align="center"style="padding:0 0 0 16px;width:36px;">
+                            <a href="https://github.com/tomascesar8/cine-rolling">
+                              <img src="https://i.ibb.co/48WGS5q/github-7.png" alt="logo-github" width="36" style="height:auto;display:block;border:0;" /> 
+                            </a>
+                          </td>
+                          <td align="center" style="padding:0 0 0 10px;width:38px;">
+                            <a href="http://www.facebook.com/">
+                              <img src="https://i.ibb.co/7zMxkKG/logo-de-facebook.png" alt="logo-Facebook" width="38" style="height:auto;display:block;border:0;" />
+                            </a>
+                          </td>
+                        </tr>
+                      </table>
+                    <td style="padding-right:25px; align:right">
+                      <a href="#" style="text-decoration:none; color:#96cae7;"><h4>Ayuda</h4></a>
+                    </td>
+                    </td>
+                  </tr>
+                </table>
+                  </td>
+                </tr>
+              </table>
+            </td>
+          </tr>
+        </table>
+      </body>
+      </html>`
+    Email.send({
+      Host : "smtp.elasticemail.com",
+      Username : "tomascesar1993@gmail.com",
+      Password : "D19478D06661B455EB35D0C3E6D2BCEC7099",
+      To : 'tomcesar@hotmail.com',
+      From : "tomascesar1993@gmail.com",
+      Subject : `Solicitud para recuperar clave CineRolling+`,
+      Body : emailRecovery,
+    }).then(message => sendConfirm(message));
+  
+    async function sendConfirm(message) {
+      let response = await message;
+      console.log('paperrrr');
+      if(response === "OK"){ 
+        console.log('Llego a if response async')
+          // alert('Llego a if response async')
+          Swal.fire({
+            position: 'center',
+            icon: 'success',
+            title: `<h3 class="text-warning">¡Te enviamos un correo electrónico para recuperar tu contraseña!</h3>
+                    <h4>Por favor revisá tu casilla de mensajes. No te olvides de buscar en Spam o Correo no deseado.</h4>
+                    `,
+            showConfirmButton: false,
+            timer: 6000,
+            showCloseButton: true,
+            background: '#0A1A2A',
+            color: 'white',
+            timerProgressBar: true,
+            width: '600px',
+            didOpen: (toast) => {
+                  toast.addEventListener('mouseenter', Swal.stopTimer)
+                  toast.addEventListener('mouseleave', Swal.resumeTimer)
+                }
+          })
+      }else{
+          let error = document.createElement('h5');
+          error.innerText = 'Ha ocurrido un problema, vuelve a intentarlo.' ;
+          console.log('Error en else async');
+          alert('Error en else async')
+          console.log(error);
+          alert(error)
+      }
+    }
+    
+  }
+}
